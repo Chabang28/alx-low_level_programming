2b236c0f21002edcffbebdbaa6f69b2eab66fc14
Chabang28 <chabangmnisi@gmail.com>
2024-07-16 01:54:31 -0700

    Add node

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,33 +1,21 @@
-
- 29 changes: 29 additions & 0 deletions29  
-0x17-doubly_linked_lists/0-print_dlistint.c
-@@ -0,0 +1,29 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints elements of list.
- * @h: pointer to the head of the list
+ * dlistint_len - number of element in a linkedlist
+ * @h: head
  *
- * Return: number of nodes
+ * Return: number of element
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-
-	int counter = 0;
-
-	if (h == NULL)
-		return (counter);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	size_t node_num = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		node_num += 1;
 		h = h->next;
-		counter++;
 	}
 
-	return (counter);
+	return (node_num);
 }
